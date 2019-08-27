9a8e6c6b50ad0eb0858f84bbb4b828568cbb2fb1
lethalSid <senien.hicks@gmail.com>
2019-08-27 16:55:11 -0400

    added more variables to the text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,33 @@ var wins;
 var losses;
 var guessesLeft;
 var guessesSoFar;
+var guessesUser;
 
-var computersLetter = [q, a, z, p, l, m, t, g, v];
+var computersLetter = ["q", "a", "z", "p", "l", "m", "t", "g", "v"];
 
-var winsText = document.getElementById(wins - text);
-var lossesText = document.getElementById(losses - text);
-var leftGuesses = document.getElementById(leftGuesses - text);
-var guessesLeftSoFar = document.getElementById(guessesLeftSoFar - text);
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessesSoFar = [];
+
+//favTVshows.push(tvShow); pushes entries to an array
+
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var leftGuessesTest = document.getElementById("leftGuesses- text");
+var guessesLeftSoFarText = document.getElementById("guessesLeftSoFar-text");
+
+function init() {}
+
+document.onkeyup = function(event) {
+	guessUser = event.key;
+	guessUser.push(guessesSoFar);
+};
+
+init();
+
+// Display the user and computer guesses, and wins/losses/ties.
+winsText.textContent = "Wins: " + wins;
+lossesText.textContent = "Losses: " + losses;
+leftGuessesText.textContent = "You have " + guessesLeft + " left.";
+guessesLeftSoFarText.textContent = "Guesses so far:" + guessesSoFar[i];
