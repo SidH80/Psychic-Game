fcb3eaae3444465efce13c997c32ea0379fef391
lethalSid <senien.hicks@gmail.com>
2019-08-27 16:10:42 -0400

    set variables in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var wins;
+var losses;
+var guessesLeft;
+var guessesSoFar;
+
+var computersLetter = [q, a, z, p, l, m, t, g, v];
+
+var winsText = document.getElementById(wins - text);
+var lossesText = document.getElementById(losses - text);
+var leftGuesses = document.getElementById(leftGuesses - text);
+var guessesLeftSoFar = document.getElementById(guessesLeftSoFar - text);
