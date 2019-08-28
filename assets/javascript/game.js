b41ee08df34d8a82c0391d8b6a27cc56b6fa361d
lethalSid <senien.hicks@gmail.com>
2019-08-28 15:58:36 -0400

    added variables and functions for javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,58 @@
-var wins;
-var losses;
-var guessesLeft;
-var guessesSoFar;
-var guessesUser;
+console.log("hooked up");
 
-var computersLetter = ["q", "a", "z", "p", "l", "m", "t", "g", "v"];
+//Create variables to store my values
+var guessUser;
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var computerGuess;
 var guessesSoFar = [];
+var computerChoices = ["q", "a", "z", "p", "l", "m", "t", "g", "v"];
+var displayText = "this is a test";
 
-//favTVshows.push(tvShow); pushes entries to an array
+//Create function to display th results to page
+function updateDisplay() {
+	document.querySelector("#displayText").innerHTML = displayText;
+	document.querySelector("#wins").innerHTML = wins;
+	document.querySelector("#losses").innerHTML = losses;
+	document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+	document.querySelector("#guessesSoFar").innerHTML = guessesSoFar;
+}
 
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var leftGuessesTest = document.getElementById("leftGuesses- text");
-var guessesLeftSoFarText = document.getElementById("guessesLeftSoFar-text");
+updateDisplay();
 
-function init() {}
+//Create a function to get a random letter for the computer
+
+function computerRandomLetter() {
+	computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
+
+computerRandomLetter();
+
+console.log(computerGuess);
+
+//Reset my scores
+function resetScores() {
+	guessesLeft = 9;
+	guessesSoFar = [];
+}
+
+computerRandomLetter();
+//create start onkey event function to grab userGuess
 
 document.onkeyup = function(event) {
-	guessUser = event.key;
-	guessUser.push(guessesSoFar);
+	userGuess = event.key;
+	console.log(userGuess);
+
+	updateDisplay();
 };
 
-// Display the user and computer guesses, and wins/losses/ties.
-winsText.textContent = "Wins: " + wins;
-lossesText.textContent = "Losses: " + losses;
-leftGuessesText.textContent = "You have " + guessesLeft + " left.";
-guessesLeftSoFarText.textContent = "Guesses so far:" + guessesSoFar[i];
+// if my guess equals computer guess then increment wins++, and fire the program again
+//if my guess doesn't equal computer guess 1. add my guess to the array 2. decrease guesses left by 1
+// if guesses left equals 0 the display game over
+//if guess equals computer guess the fire next round
+
+//
+
+//favTVshows.push(tvShow); pushes entries to an array
