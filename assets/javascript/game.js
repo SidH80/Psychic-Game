3fa35c2d32dd7a8485da70af824b53ea3956d6bb
lethalSid <senien.hicks@gmail.com>
2019-08-28 20:06:17 -0400

    game works 100%

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 console.log("hooked up");
 
 //Create variables to store my values
-var guessUser;
+var userGuess;
 
 var wins = 0;
 var losses = 0;
@@ -48,16 +48,12 @@ function updateDisplay() {
 	document.querySelector("#guessesSoFar").innerHTML = guessesSoFar;
 }
 
-updateDisplay();
-
 //Create a function to get a random letter for the computer
-
 function computerRandomLetter() {
 	computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 
 computerRandomLetter();
-
 console.log(computerGuess);
 
 //Reset my scores
@@ -65,8 +61,6 @@ function resetScores() {
 	guessesLeft = 9;
 	guessesSoFar = [];
 }
-
-computerRandomLetter();
 
 //create start onkey event function to grab userGuess
 document.onkeyup = function(event) {
@@ -76,16 +70,17 @@ document.onkeyup = function(event) {
 	//Display user guess and computer guess
 	displayText = "You chose: " + userGuess;
 
-	// if my guess equals computer guess then increment wins+
+	// if my guess equals computer guess then increment wins+, have computer guess again
 	if (userGuess === computerGuess) {
-		console.log("You got it!");
+		displayText = "You got it!!!";
 		wins++;
-		// if my guess does not equal computer guess then decrease losses by 1 and push userGuess to guesseSoFar array
+
+		// if my guess does not equal computer guess then decrease losses by 1 and push userGuess to guesseSoFar array and have computer guess again
 	} else {
 		guessesSoFar.push(userGuess);
 		guessesLeft--;
 	}
-
+	//after 9 guesses display "play again?", increase losses by 1, reset scores
 	if (guessesLeft === 0) {
 		console.log("try again");
 		displayText = "You loose. Another round?";
@@ -93,15 +88,7 @@ document.onkeyup = function(event) {
 		resetScores();
 	}
 
+	computerRandomLetter();
+	console.log(computerGuess);
 	updateDisplay();
 };
-
-// if my guess equals computer guess then increment wins++, and fire the program again
-
-//if my guess doesn't equal computer guess 1. add my guess to the array 2. decrease guesses left by 1
-// if guesses left equals 0 the display game over
-//if guess equals computer guess the fire next round
-
-//
-
-//favTVshows.push(tvShow); pushes entries to an array
